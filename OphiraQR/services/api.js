afe1aaacfa9e60a7078ce08b7119b5f34a4ad89e
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2026-03-26 19:38:48 -0600

    creado eln api.js

---
+++ OphiraQR/services/api.js
@@ -0,0 +1,47 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const API_URL = "http://localhost:4000/api"; // todo: no se si jale con localhost
+
+export const api = {
+  get: async (endpoint) => {
+    const token = await AsyncStorage.getItem('token');
+
+    try {
+      const response = await fetch(`${API_URL}${endpoint}`, {
+        headers: {
+          'Authorization': token ? `Bearer ${token}` : '',
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+      return await response.json();
+
+    } catch (error) {
+      console.error("Error en GET:", error);
+      throw error;
+    }
+  },
+
+  post: async (endpoint, body) => {
+    const token = await AsyncStorage.getItem('token');
+
+    try {
+      const response = await fetch(`${API_URL}${endpoint}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token ? `Bearer ${token}` : ''
+        },
+        body: JSON.stringify(body)
+      });
+
+      if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+      return await response.json();
+
+    } catch (error) {
+      console.error("Error en POST:", error);
+      throw error;
+    }
+  }
+};
